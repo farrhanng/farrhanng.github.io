e1d9ff2cd40a21d64d66c408a1969243c0339a55
Farrhan <work.farrhan@gmail.com>
2024-01-23 15:50:29 +0800

    init5

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/farrhanng.github.io/', // Replace with your GitHub repository name
+  base: '/',
   plugins: [react()],
 })
